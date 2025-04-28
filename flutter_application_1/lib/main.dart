46676a2e8438989c53139b72e1ae670639d182d3
nathangerardeaux <nathanbglol050606@gmail.com>
2025-04-28 10:07:38 +0200

    zd

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -7,7 +7,7 @@ void main() {
 }
 
 class ProfilePage extends StatelessWidget {
-  const ProfilePage({Key? key}) : super(key: key);
+  const ProfilePage({super.key});
 
   @override
   Widget build(BuildContext context) {
